a6c783a19adc5a1e61b7531d7c9435ff21931bf0
Nathanael <nael.nathanael71@gmail.com>
2020-05-19 07:09:29 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Pur-for-me
+
+Enterprise Purchasing App, Integrated with Odoo Community v12.0 \
+[General development idea]()
+
+## Getting Started
+
+Initialized using Flutter Dev
+
+## Author
+
+[Nathanael](http://nael.nathanael.my.id/) - Fakultas Ilmu Komputer, Universitas Indonesia
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+	runApp(
+		MaterialApp(
+			title: "Purchase for Me",
+			home: Scaffold(
+				appBar: AppBar(
+					title: Text("Purchase for Me"),
+					centerTitle: true,
+				),
+				body: Container(
+					child: Text("Hallo"),
+				)
+			),
+		)
+	);
+}
