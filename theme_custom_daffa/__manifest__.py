1fd84ddb16c2a66997768fd0f5f777d9f5b5fe36
dafpool4 <60530643+dafpool4@users.noreply.github.com>
2020-07-14 15:09:55 +0700

    TRY

---
+++ theme_custom_daffa/__manifest__.py
@@ -0,0 +1,22 @@
+# © 2015 Oihane Crucelaegui - AvanzOSC
+# License AGPL-3 - See https://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Theme Custom Daffa",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Ananda Daffa Sabila Isliyana",
+    "website": "",
+    "category": "Theme/Creative",
+    "depends": [
+        "website",
+		"website_theme_install",
+    ],
+    "data": [
+	"views/layout.xml",
+	"views/pages.xml",
+	"views/snippets.xml",
+	"views/options.xml",
+    ],
+    "installable": True
+}
